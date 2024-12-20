e99b53186679f113251121173bd61b93bfe95da6
Mert Güler <108007620+mertgulerx@users.noreply.github.com>
2024-12-20 13:59:13 +0300

    testing javafx directory

---
+++ src/main/java/mertguler/HelloController.java
@@ -0,0 +1,14 @@
+package mertguler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome Hospital Reservation System");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module mertguler.hospitalreservationsystemjava {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens mertguler to javafx.fxml;
+    exports mertguler;
+}
